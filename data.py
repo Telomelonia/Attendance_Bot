4bec4fd56d890b380f781c9ea759800857347e36
telomelonia <shuklas2002@gmail.com>
2024-03-05 11:29:44 +0530

    data entry

--- data.py
+++ data.py
@@ -2,9 +2,15 @@ usernames = [
     "20118016",
     "20310011",
     "20118073",
+    "20118029",
+    "20118060",
+    "20118017"
 ]
-passwords = [
+passwords = [   
     "lol1234",
     "sasageyo",
     "Sak@#2512",
+    "divyagrawal29",
+    "prateek1",
+    "ashish@roorkee"
 ]
