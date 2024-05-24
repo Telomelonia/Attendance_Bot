f7dec31ee1c62cc9da866210837a7c5d5da5f622
telomelonia <shuklas2002@gmail.com>
2024-05-24 21:41:12 +0530

    chrome extension functionality added

---
+++ chrome_extension/attendance_ext/content.js
@@ -0,0 +1,12 @@
+function clickButtonInNthRow(rowIndex) {
+    const table = document.getElementById('customers');
+    const rows = table.querySelectorAll('tr');
+    const row = rows[rowIndex]; 
+    const lastTd = row.querySelector('td:last-child');
+    const button = lastTd.querySelector('button.btn.btn-primary');
+    if (button) {
+        button.click();
+    }
+}
+
+setInterval(() => clickButtonInNthRow(1), 10000);
